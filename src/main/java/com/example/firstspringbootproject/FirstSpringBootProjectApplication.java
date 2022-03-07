ab982c2da3b85201f6e077a1c30f18fd407bbee3
aozcann <ozcanahmet94@gmail.com>
2022-03-07 20:48:50 +0300

    first commit

---
+++ src/main/java/com/example/firstspringbootproject/FirstSpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootProjectApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
