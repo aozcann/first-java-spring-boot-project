c291bef35ae4f2b3e21c12423a9e953bf8502120
aozcann <ozcanahmet94@gmail.com>
2022-03-13 19:42:25 +0300

    Trainer Crud operations implemented

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO
-	customer (name, surname, email, phone_number)
-VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com', '55555555555'),
-  	('John', 'Doe', 'xyz@email.com', '5551111111');
